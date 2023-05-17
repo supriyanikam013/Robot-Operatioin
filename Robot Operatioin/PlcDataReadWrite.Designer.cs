fee32299c8315216b15a7d3d6691df809354bb1e
supriya nikam <supriyanikam013@gmail.com>
2023-05-17 16:01:10 +0530

    plc connection demo form added.

--- Robot Operatioin/PlcDataReadWrite.Designer.cs
+++ Robot Operatioin/PlcDataReadWrite.Designer.cs
@@ -29,12 +29,112 @@ namespace Robot_Operatioin
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblCommunicationStatus = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtvalue = new System.Windows.Forms.TextBox();
+            this.btnwrite = new System.Windows.Forms.Button();
+            this.btnread = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCommunicationStatus
+            // 
+            this.lblCommunicationStatus.BackColor = System.Drawing.Color.Gray;
+            this.lblCommunicationStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCommunicationStatus.ForeColor = System.Drawing.Color.Gray;
+            this.lblCommunicationStatus.Location = new System.Drawing.Point(192, 58);
+            this.lblCommunicationStatus.Name = "lblCommunicationStatus";
+            this.lblCommunicationStatus.Size = new System.Drawing.Size(28, 20);
+            this.lblCommunicationStatus.TabIndex = 586;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(180, 25);
+            this.label3.TabIndex = 585;
+            this.label3.Text = "Check connection :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 25);
+            this.label1.TabIndex = 584;
+            this.label1.Text = "Text Value";
+            // 
+            // txtvalue
+            // 
+            this.txtvalue.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtvalue.Location = new System.Drawing.Point(159, 92);
+            this.txtvalue.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtvalue.Name = "txtvalue";
+            this.txtvalue.Size = new System.Drawing.Size(232, 30);
+            this.txtvalue.TabIndex = 583;
+            // 
+            // btnwrite
+            // 
+            this.btnwrite.Location = new System.Drawing.Point(269, 140);
+            this.btnwrite.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnwrite.Name = "btnwrite";
+            this.btnwrite.Size = new System.Drawing.Size(122, 40);
+            this.btnwrite.TabIndex = 582;
+            this.btnwrite.Text = "Write";
+            this.btnwrite.UseVisualStyleBackColor = true;
+            // 
+            // btnread
+            // 
+            this.btnread.Location = new System.Drawing.Point(159, 140);
+            this.btnread.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnread.Name = "btnread";
+            this.btnread.Size = new System.Drawing.Size(101, 40);
+            this.btnread.TabIndex = 581;
+            this.btnread.Text = "Read";
+            this.btnread.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnwrite);
+            this.groupBox1.Controls.Add(this.lblCommunicationStatus);
+            this.groupBox1.Controls.Add(this.btnread);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtvalue);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(407, 258);
+            this.groupBox1.TabIndex = 587;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // PlcDataReadWrite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(431, 287);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "PlcDataReadWrite";
             this.Text = "PlcDataReadWrite";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        internal System.Windows.Forms.Label lblCommunicationStatus;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtvalue;
+        private System.Windows.Forms.Button btnwrite;
+        private System.Windows.Forms.Button btnread;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
